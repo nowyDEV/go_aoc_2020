430ab263ab9d0e425cbcde640c613ddb96e98f10
nowyDEV <dominik.nowik@nordcloud.com>
2020-12-16 23:01:45 +0100

    day 10 part 1 solution

---
+++ day_10/solution.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"aoc-2020/utils"
+	"fmt"
+	"math"
+	"sort"
+)
+
+func main() {
+	data := utils.ReadTextFile("./data_test.txt")
+
+	numbers := make([]int, len(data))
+	for index, item := range data {
+		numbers[index] = utils.GetIntFromString(item)
+	}
+
+	voltageArr := append([]int{0}, numbers...)
+	sort.Ints(voltageArr)
+
+	fmt.Println(voltageArr)
+
+
+	fmt.Println(multiplyJoltDiffs(voltageArr))
+	fmt.Println(getArrangements(voltageArr))
+}
+
+func getArrangements(voltageArr []int) int {
+	numOfIntermediates := 0
+	
+	for i := 0; i < len(voltageArr)-2; i++ {
+		diff := int(math.Abs(float64(voltageArr[i] - voltageArr[i+2])))
+
+		if (diff <= 3) {
+			numOfIntermediates++
+		}
+	}
+
+	fmt.Println(numOfIntermediates)
+
+
+	return int(math.Pow(2, float64(numOfIntermediates)))
+}
+
+func multiplyJoltDiffs(voltageArr []int) int {
+	var numOfThrees = 0
+	var numOfOnes = 0
+
+	for i := 0; i < len(voltageArr)-1; i++ {
+		diff := voltageArr[i+1] - voltageArr[i]
+
+		if diff == 1 {
+			numOfOnes++
+		} else if diff == 3 {
+			numOfThrees++
+		}
+	}
+
+	numOfThrees++
+
+	return numOfOnes * numOfThrees
+}
