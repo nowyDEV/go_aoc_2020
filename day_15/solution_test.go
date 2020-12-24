50e8d306c8e01ac6ee5e2cafcd671735231ad55f
nowyDEV <dominik.nowik@nordcloud.com>
2020-12-24 13:22:08 +0100

    day 15 part 1 solution

---
+++ day_15/solution_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"aoc-2020/utils"
+	"strings"
+	"testing"
+)
+
+// BenchmarkBigLen Test it
+func BenchmarkBigLen(b *testing.B) {
+	data := utils.ReadTextFile("./data.txt")
+
+	var numbers []int
+
+	for _, numStr := range strings.Split(data[0], ",") {
+		numbers = append(numbers, utils.GetIntFromString(numStr))
+	}
+	
+	getNthNumber(numbers, 30000-len(numbers))
+}
